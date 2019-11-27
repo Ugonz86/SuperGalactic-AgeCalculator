d1f37dc27e91572a7ab37e7da06747bfeb915d0c
Ugonz86 <ugonzalez86@gmail.com>
2019-11-27 15:55:37 -0800

    Add new function that calculates next birthday in other planets.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import './styles.css';
 import $ from 'jquery';
 import { GalacticCalculator } from './backend-code';
 import { ExpectancyCalculator } from './backend-code';
-// import { BirthdayCalculator } from './backend-code';
+import { NextBirthdayCalculator } from './backend-code';
 import "./img/mercury.jpg";
 import "./img/venus.jpg";
 import "./img/mars.jpg";
@@ -19,13 +19,14 @@ $(document).ready(function(){
     $("#expectancyForm").slideDown("slow");
     $("#intro").hide("slow");
   });
-  // $("#seeNextBirthday").click(function() {
-  //   $("#birthdayForm").slideDown("slow");
-  //   $("#intro").hide("slow");
-  // });
+  $("#seeNextBirthday").click(function() {
+    $("#birthdayForm").slideDown("slow");
+    $("#intro").hide("slow");
+  });
   $(".goHome").click(function() {
     location.reload();
   });
+
   $("#ageInputForm").submit(function(event) {
     event.preventDefault();
     var inputtedAge = $('#ageInput').val();
@@ -38,6 +39,7 @@ $(document).ready(function(){
     $("#ageInMars").text(calculatorResult.ageInMarsYears());
     $("#ageInJupiter").text(calculatorResult.ageInJupiterYears());
   });
+
   $("#expectancyForm").submit(function(event) {
     event.preventDefault();
     var inputtedLifeExpectancy = $('#expectancyInput').val();
@@ -67,7 +69,6 @@ $(document).ready(function(){
     else {
       $("#yearsLeftInMars").text("Years past life expectancy in Mars: " + Math.abs(expectancyInMars));
     }
-
     var expectancyInJupiter = calculatorResult.lifeExpectancyInJupiter();
     if (expectancyInJupiter >= 0) {
       $("#yearsLeftInJupiter").text("Years left in Jupiter: " + expectancyInJupiter);
@@ -76,8 +77,23 @@ $(document).ready(function(){
       $("#yearsLeftInJupiter").text("Years past life expectancy in Jupiter: " + Math.abs(expectancyInJupiter));
     }
   });
-  // $("#birthdayForm").submit(function(event) {
-  //   event.preventDefault();
-  //   var inputtedBirthday = $('#birthdayInput').val();
-  // });
+
+  $("#birthdayForm").submit(function(event) {
+    event.preventDefault();
+    var inputtedDay = $('#dayInput').val();
+    var inputtedMonth = $('#monthInput').val();
+    var inputtedYear = $('#yearInput').val();
+    var calculatorResult = new NextBirthdayCalculator (inputtedDay, inputtedMonth, inputtedYear);
+    event.preventDefault();
+    $("#birthdayForm").hide("slow");
+    $("#nextBirthdayInPlanets").slideDown("slow");
+    var nextBdayInMercury = calculatorResult.nextBirthdayInMercury();
+    $("#birthdayInMercury").text("Your next birthday: " + nextBdayInMercury);
+    var nextBdayInVenus = calculatorResult.nextBirthdayInVenus();
+    $("#birthdayInVenus").text("Your next birthday: " + nextBdayInVenus);
+    var nextBdayInMars = calculatorResult.nextBirthdayInMars();
+    $("#birthdayInMars").text("Your next birthday: " + nextBdayInMars);
+    var nextBdayInJupiter = calculatorResult.nextBirthdayInJupiter();
+    $("#birthdayInJupiter").text("Your next birthday: " + nextBdayInJupiter);
+  });
 });
